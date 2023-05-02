3fd38cd6ad62ab61acdb2b905c69d264caacf750
LudmilaS <ludmila.https@gmail.com>
2023-05-02 16:24:39 -0300

    feat: bookingput

--- src/repositories/booking-repository/index.ts
+++ src/repositories/booking-repository/index.ts
@@ -1,34 +1,53 @@
 import { prisma } from '@/config';
 
 async function findBookingByRoomId(roomId: number) {
-  const booking = await prisma.booking.findFirst({
-    where: {
-      roomId: roomId,
-    },
-  });
-  return booking.id;
+    return await prisma.booking.findFirst({
+        where: {
+            roomId: roomId,
+        },
+    });
 }
 
 async function createBooking(roomId: number, userId: number) {
-  const booking = await prisma.booking.create({
-    data: {
-      roomId: roomId,
-      userId: userId,
-    },
-  });
-  return booking.id;
+    return await prisma.booking.create({
+        data: {
+            roomId: roomId,
+            userId: userId,
+        },
+    });
 }
 
 async function findBookingByUserId(userId: number) {
     return await prisma.booking.findFirst({
-      where: {
-        userId: userId,
-      },
+        where: {
+            userId: userId,
+        },
     });
-  }
+}
+
+async function findBookingById(bookingId: number) {
+    return await prisma.booking.findFirst({
+        where: {
+            id: bookingId,
+        },
+    });
+}
+
+async function updateBookingById(bookingId: number, roomId: number) {
+    return await prisma.booking.update({
+        where: {
+            id: bookingId,
+        },
+        data: {
+            roomId: roomId,
+        },
+    });
+}
 
 export default {
-  findBookingByRoomId,
-  createBooking,
-  findBookingByUserId,
+    findBookingByRoomId,
+    createBooking,
+    findBookingByUserId,
+    findBookingById,
+    updateBookingById,
 };
