8953c9f81132d0b778df934664fe04f777290a5f
LudmilaS <ludmila.https@gmail.com>
2023-07-03 16:20:54 -0300

    feat: update

--- src/schemas/enrollments-schemas.ts
+++ src/schemas/enrollments-schemas.ts
@@ -8,6 +8,25 @@ const cepValidationSchema = Joi.string().length(9).custom(JoiCepValidation).requ
 
 const mobilePhoneValidationSchema = Joi.string().min(14).max(15).custom(joiMobilePhoneValidation).required();
 
+export const createOrUpdateEnrollmentSchema = Joi.object<CreateOrUpdateEnrollmentWithAddress>({
+  name: Joi.string().min(3).required(),
+  cpf: cpfValidationSchema,
+  birthday: Joi.string().isoDate().required(),
+  phone: mobilePhoneValidationSchema,
+  address: Joi.object({
+    cep: cepValidationSchema,
+    street: Joi.string().required(),
+    city: Joi.string().required(),
+    number: Joi.string().required(),
+    state: Joi.string()
+      .length(2)
+      .valid(...getStates().map((s) => s.code))
+      .required(),
+    neighborhood: Joi.string().required(),
+    addressDetail: Joi.string().allow(null, ''),
+  }).required(),
+});
+
 export const createEnrollmentSchema = Joi.object<CreateOrUpdateEnrollmentWithAddress>({
   name: Joi.string().min(3).required(),
   cpf: cpfValidationSchema,
