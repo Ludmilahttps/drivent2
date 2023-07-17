9e010bf5d7dc66ff53faf0700b93c61bbbeca736
LudmilaS <ludmila.https@gmail.com>
2023-07-17 18:15:39 -0300

    feat: update

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -6,6 +6,7 @@ import { createSession } from './factories/sessions-factory';
 import { prisma } from '@/config';
 
 export async function cleanDb() {
+  await prisma.booking.deleteMany({});
   await prisma.address.deleteMany({});
   await prisma.payment.deleteMany({});
   await prisma.ticket.deleteMany({});
@@ -14,6 +15,8 @@ export async function cleanDb() {
   await prisma.session.deleteMany({});
   await prisma.user.deleteMany({});
   await prisma.ticketType.deleteMany({});
+  await prisma.room.deleteMany({});
+  await prisma.hotel.deleteMany({});
 }
 
 export async function generateValidToken(user?: User) {
